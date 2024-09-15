2489857e7610a2cce6239407ad5ba0594bf520c6
David Torres <143417909+dtorresev@users.noreply.github.com>
2024-09-15 16:08:57 -0600

    s

--- Mid-term/Mid-term.py
+++ Mid-term/Mid-term.py
@@ -6,6 +6,13 @@ img_type_flag  = 1
 filename = "traffic.jpeg"
 img = cv2.imread(filename,img_type_flag)
 
+# Check the current working directory
+print("Current working directory:", os.getcwd())
+
+# Check if the image file exists at the given path
+#image_path = "path/to/your/image.jpg"
+#print("Image exists:", os.path.exists(image_path))
+
 if img is None:
     print("Error: Could not open or find the image.")
 else:
